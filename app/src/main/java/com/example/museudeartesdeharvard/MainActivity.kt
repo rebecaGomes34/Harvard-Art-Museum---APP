478a70fb76f1dba17c72b3093281f1210c5c48e0
Rebeca Gomes <rebeca0606072@gmail.com>
2026-01-06 23:34:38 -0300

    Implementa tela de splash com animações para carregamento inicial

---
+++ app/src/main/java/com/example/museudeartesdeharvard/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.museudeartesdeharvard
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val imageQuadros = findViewById<ImageView>(R.id.quadros)
+        val textTema= findViewById<TextView>(R.id.tema)
+
+        val topAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation:Animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        bottomAnimation.startOffset = 800
+        imageQuadros.startAnimation(topAnimation)
+        textTema.startAnimation(bottomAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, Cadastro_Activity::class.java)
+
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                R.anim.fade_in_animation,
+                R.anim.fade_out_animation
+            )
+
+            startActivity(intent, options.toBundle())
+            finish()
+
+            }, 3300)
+
+    }
+}
