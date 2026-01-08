b19feeb0156c100e0e93fa1e7d7b3d400cea269d
Rebeca Gomes <rebeca0606072@gmail.com>
2026-01-08 15:23:38 -0300

    feat(auth): add forgot password activity and layout

---
+++ app/src/main/java/com/example/museudeartesdeharvard/RedefinirSenha_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.museudeartesdeharvard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class RedefinirSenha_Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recuperar_senha)
+    }}
