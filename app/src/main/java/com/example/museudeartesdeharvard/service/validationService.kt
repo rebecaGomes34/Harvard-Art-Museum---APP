eabdbed77c9e61276027e538425767aaac5f94a4
Rebeca Gomes <rebeca0606072@gmail.com>
2026-01-08 17:24:10 -0300

    feat(auth): add strong password validation rules

--- app/src/main/java/com/example/museudeartesdeharvard/service/validationService.kt
+++ app/src/main/java/com/example/museudeartesdeharvard/service/validationService.kt
@@ -22,6 +22,30 @@ object ValidationService {
         }
 
         return null
+    }
 
+    fun validatePassword(password: String): String? {
+
+        if (password.isBlank()) {
+            return "É necessário fornecer uma senha"
+        }
+
+        if (password.length < 8) {
+            return "A senha deve conter pelo menos 8 caracteres"
+        }
+
+        if (!password.any { it.isUpperCase() }) {
+            return "A senha deve conter uma letra maiúscula"
+        }
+
+        if(!password.any {it.isLowerCase()}){
+            return "A senha precisa conter uma letra minúscula"
+        }
+
+        if(!password.any{!it.isLetterOrDigit()}){
+            return "A senha deve conter um caractere especial"
+        }
+
+        return null
     }
 }
