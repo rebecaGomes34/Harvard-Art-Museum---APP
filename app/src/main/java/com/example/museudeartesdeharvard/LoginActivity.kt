fc0894b253424225a7b9380126dd0f69412ac073
Rebeca Gomes <rebeca0606072@gmail.com>
2026-01-07 21:31:44 -0300

    feat: add login screen animations and improve UI layout

--- app/src/main/java/com/example/museudeartesdeharvard/LoginActivity.kt
+++ app/src/main/java/com/example/museudeartesdeharvard/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.example.museudeartesdeharvard
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
@@ -8,6 +12,33 @@ class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val titleLogin = findViewById<TextView>(R.id.textLogin)
+        val textEmail = findViewById<TextView>(R.id.textEmail)
+        val textSenha = findViewById<TextView>(R.id.textSenha)
+        val textForgotPassword = findViewById<TextView>(R.id.textForgotPassword)
+        val textGoToRegister = findViewById<TextView>(R.id.textGoToRegister)
+
+        // Inputs
+        val inputEmail = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.editText_Email)
+        val inputSenha = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editTextSenha)
+
+        // Button
+        val buttonLogin = findViewById<Button>(R.id.buttom_fazerLogin)
+
+        // Animação (mesma da introduction)
+        val animation: Animation =
+            AnimationUtils.loadAnimation(this, R.anim.logo_animation)
+
+        // Aplicando animação em todos
+        titleLogin.startAnimation(animation)
+        textEmail.startAnimation(animation)
+        inputEmail.startAnimation(animation)
+        textSenha.startAnimation(animation)
+        inputSenha.startAnimation(animation)
+        textForgotPassword.startAnimation(animation)
+        textGoToRegister.startAnimation(animation)
+        buttonLogin.startAnimation(animation)
 
     }
         }
